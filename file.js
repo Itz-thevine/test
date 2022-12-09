7e984f8f5585a7af7394c8e0ca113cc88a2b8fb5
Ikechukwu Divine <60890161+Itz-thevine@users.noreply.github.com>
2022-12-09 20:03:56 +0100

    fjkdjfkd.txt
    # FE-129(https://linear.app/team-chisel-hng9/issue/FE-129/update-the-views-on-forum-posts)
    
    ## General Changes
    
    -   Implemented an API integration that gets one post for authenticated users
    - made the page responsive on larger screens

---
+++ file.js
@@ -0,0 +1,6 @@
+# FE-129(https://linear.app/team-chisel-hng9/issue/FE-129/update-the-views-on-forum-posts)
+
+## General Changes
+
+-   Implemented an API integration that gets one post for authenticated users
+- made the page responsive on larger screens
